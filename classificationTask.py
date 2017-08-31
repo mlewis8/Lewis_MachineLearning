f30724d53bfff41a81d3552aee931b7f9aec4322
Lewis <mlewis8@RVLAS033L-03741.local>
2017-08-30 19:38:20 -0500

    Renamed classificationTask

---
+++ classificationTask.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+fruits = pd.read_table('fruit_data_with_colors.txt')
+
+
